f541ede9fcd61b8481ba51dc3d420d23d0fdd6fc
mrkaye97 <mrkaye97@gmail.com>
2025-09-06 17:41:09 -0400

    fix: misc

--- app/components/signup-form.tsx
+++ app/components/signup-form.tsx
@@ -208,7 +208,6 @@ export function SignupForm() {
               {isLoading ? "Creating..." : "Sign Up"}
             </Text>
           </TouchableOpacity>
-          </Text>
         </ThemedView>
       </ScrollView>
     </TouchableWithoutFeedback>
